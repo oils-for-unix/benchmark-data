22e2062659a4e6edcdae19f83ad0297973eb3238
Andy Chu <andy@oilshell.org>
2017-12-20 09:04:02 -0800

    Saving reports after turning on ASDL code gen.

--- run.sh
+++ run.sh
@@ -19,4 +19,20 @@ remove-uncommitted() {
   uncommitted | xargs --no-run-if-empty -- rm -r -f --verbose
 }
 
+save-benchmarks() {
+  local dest=$1  # e.g. saved/2017-12-20
+
+  # no need to save oheap right now since everything is the same size.
+
+  # TODO: links to ../../web/benchmarks.css and ../../web/table are broken.
+  # Could just massage with sed -i '' *.html
+
+  for dir in ../oil/_tmp/{osh-parser,osh-runtime,vm-baseline}; do
+    cp -r $dir $dest
+  done
+
+  # This data doesn't belong
+  rm -r -f -v $dest/*/raw
+}
+
 "$@"
