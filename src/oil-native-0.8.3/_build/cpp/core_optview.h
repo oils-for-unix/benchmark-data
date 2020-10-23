f219b8a215c7749822a39bfaf06fdb6cc7e4899e
Andy Chu <andy@oilshell.org>
2020-10-22 23:44:00 -0700

    src for oil-native 0.8.3

---
+++ src/oil-native-0.8.3/_build/cpp/core_optview.h
@@ -0,0 +1,99 @@
+#ifndef OPTVIEW_H
+#define OPTVIEW_H
+
+#include "mylib.h"
+#include "option_asdl.h"
+
+namespace optview {
+
+namespace option_i = option_asdl::option_i;
+
+class _View {
+ public:
+  _View(List<bool>* opt0_array, List<List<bool>*>* opt_stacks)
+      : opt0_array(opt0_array), opt_stacks(opt_stacks) {
+  }
+
+  bool _Get(int opt_num) {
+    List<bool>* overlay = opt_stacks->index(opt_num);
+    if ((overlay == nullptr) or len(overlay) == 0) {
+      return opt0_array->index(opt_num);
+    } else {
+      return overlay->index(-1);
+    }
+  }
+
+  List<bool>* opt0_array;
+  List<List<bool>*>* opt_stacks;
+};
+
+class Parse : public _View {
+ public:
+  Parse(List<bool>* opt0_array, List<List<bool>*>* opt_stacks)
+      : _View(opt0_array, opt_stacks) {
+  }
+  bool parse_dynamic_arith() { return _Get(option_i::parse_dynamic_arith); }
+  bool strict_backslash() { return _Get(option_i::strict_backslash); }
+  bool strict_backticks() { return _Get(option_i::strict_backticks); }
+  bool strict_dollar() { return _Get(option_i::strict_dollar); }
+  bool parse_at() { return _Get(option_i::parse_at); }
+  bool parse_brace() { return _Get(option_i::parse_brace); }
+  bool parse_index_expr() { return _Get(option_i::parse_index_expr); }
+  bool parse_paren() { return _Get(option_i::parse_paren); }
+  bool parse_rawc() { return _Get(option_i::parse_rawc); }
+  bool parse_ignored() { return _Get(option_i::parse_ignored); }
+  bool parse_set() { return _Get(option_i::parse_set); }
+  bool parse_equals() { return _Get(option_i::parse_equals); }
+  bool parse_at_all() { return _Get(option_i::parse_at_all); }
+  bool parse_tea() { return _Get(option_i::parse_tea); }
+};
+
+#ifndef OSH_PARSE  // hack for osh_parse, set in build/mycpp.sh
+class Exec : public _View {
+ public:
+  Exec(List<bool>* opt0_array, List<List<bool>*>* opt_stacks)
+      : _View(opt0_array, opt_stacks) {
+  }
+  bool errexit() { return _Get(option_i::errexit); }
+  bool nounset() { return _Get(option_i::nounset); }
+  bool hashall() { return _Get(option_i::hashall); }
+  bool pipefail() { return _Get(option_i::pipefail); }
+  bool noexec() { return _Get(option_i::noexec); }
+  bool xtrace() { return _Get(option_i::xtrace); }
+  bool verbose() { return _Get(option_i::verbose); }
+  bool noglob() { return _Get(option_i::noglob); }
+  bool noclobber() { return _Get(option_i::noclobber); }
+  bool posix() { return _Get(option_i::posix); }
+  bool vi() { return _Get(option_i::vi); }
+  bool emacs() { return _Get(option_i::emacs); }
+  bool interactive() { return _Get(option_i::interactive); }
+  bool failglob() { return _Get(option_i::failglob); }
+  bool extglob() { return _Get(option_i::extglob); }
+  bool eval_unsafe_arith() { return _Get(option_i::eval_unsafe_arith); }
+  bool compat_array() { return _Get(option_i::compat_array); }
+  bool verbose_errexit() { return _Get(option_i::verbose_errexit); }
+  bool allow_command_sub() { return _Get(option_i::allow_command_sub); }
+  bool nullglob() { return _Get(option_i::nullglob); }
+  bool inherit_errexit() { return _Get(option_i::inherit_errexit); }
+  bool strict_argv() { return _Get(option_i::strict_argv); }
+  bool strict_arith() { return _Get(option_i::strict_arith); }
+  bool strict_array() { return _Get(option_i::strict_array); }
+  bool strict_control_flow() { return _Get(option_i::strict_control_flow); }
+  bool strict_echo() { return _Get(option_i::strict_echo); }
+  bool strict_errexit() { return _Get(option_i::strict_errexit); }
+  bool strict_eval_builtin() { return _Get(option_i::strict_eval_builtin); }
+  bool strict_nameref() { return _Get(option_i::strict_nameref); }
+  bool strict_word_eval() { return _Get(option_i::strict_word_eval); }
+  bool strict_tilde() { return _Get(option_i::strict_tilde); }
+  bool strict_glob() { return _Get(option_i::strict_glob); }
+  bool simple_word_eval() { return _Get(option_i::simple_word_eval); }
+  bool dashglob() { return _Get(option_i::dashglob); }
+  bool command_sub_errexit() { return _Get(option_i::command_sub_errexit); }
+  bool process_sub_fail() { return _Get(option_i::process_sub_fail); }
+  bool simple_test_builtin() { return _Get(option_i::simple_test_builtin); }
+};
+#endif  // OSH_PARSE
+
+}  // namespace optview
+
+#endif  // OPTVIEW_H
---
+++ src/oil-native-0.8.3/_build/cpp/option_asdl.h
@@ -0,0 +1,176 @@
+#ifndef OPTION_ASDL_H
+#define OPTION_ASDL_H
+
+namespace option_asdl {
+namespace option_i {
+  const int errexit = 1;
+  const int nounset = 2;
+  const int hashall = 3;
+  const int pipefail = 4;
+  const int noexec = 5;
+  const int xtrace = 6;
+  const int verbose = 7;
+  const int noglob = 8;
+  const int noclobber = 9;
+  const int posix = 10;
+  const int vi = 11;
+  const int emacs = 12;
+  const int interactive = 13;
+  const int failglob = 14;
+  const int extglob = 15;
+  const int eval_unsafe_arith = 16;
+  const int parse_dynamic_arith = 17;
+  const int compat_array = 18;
+  const int verbose_errexit = 19;
+  const int allow_command_sub = 20;
+  const int nullglob = 21;
+  const int inherit_errexit = 22;
+  const int strict_argv = 23;
+  const int strict_arith = 24;
+  const int strict_array = 25;
+  const int strict_control_flow = 26;
+  const int strict_echo = 27;
+  const int strict_errexit = 28;
+  const int strict_eval_builtin = 29;
+  const int strict_nameref = 30;
+  const int strict_word_eval = 31;
+  const int strict_tilde = 32;
+  const int strict_glob = 33;
+  const int strict_backslash = 34;
+  const int strict_backticks = 35;
+  const int strict_dollar = 36;
+  const int simple_word_eval = 37;
+  const int dashglob = 38;
+  const int command_sub_errexit = 39;
+  const int process_sub_fail = 40;
+  const int simple_test_builtin = 41;
+  const int parse_at = 42;
+  const int parse_brace = 43;
+  const int parse_index_expr = 44;
+  const int parse_paren = 45;
+  const int parse_rawc = 46;
+  const int parse_ignored = 47;
+  const int parse_set = 48;
+  const int parse_equals = 49;
+  const int parse_at_all = 50;
+  const int parse_tea = 51;
+  const int expand_aliases = 52;
+  const int lastpipe = 53;
+  const int progcomp = 54;
+  const int histappend = 55;
+  const int hostcomplete = 56;
+  const int cmdhist = 57;
+  const int assoc_expand_once = 58;
+  const int autocd = 59;
+  const int cdable_vars = 60;
+  const int cdspell = 61;
+  const int checkhash = 62;
+  const int checkjobs = 63;
+  const int checkwinsize = 64;
+  const int complete_fullquote = 65;
+  const int direxpand = 66;
+  const int dirspell = 67;
+  const int dotglob = 68;
+  const int execfail = 69;
+  const int extdebug = 70;
+  const int extquote = 71;
+  const int force_fignore = 72;
+  const int globasciiranges = 73;
+  const int globstar = 74;
+  const int gnu_errfmt = 75;
+  const int histreedit = 76;
+  const int histverify = 77;
+  const int huponexit = 78;
+  const int interactive_comments = 79;
+  const int lithist = 80;
+  const int localvar_inherit = 81;
+  const int localvar_unset = 82;
+  const int login_shell = 83;
+  const int mailwarn = 84;
+  const int no_empty_cmd_completion = 85;
+  const int nocaseglob = 86;
+  const int nocasematch = 87;
+  const int progcomp_alias = 88;
+  const int promptvars = 89;
+  const int restricted_shell = 90;
+  const int shift_verbose = 91;
+  const int sourcepath = 92;
+  const int xpg_echo = 93;
+  const int ARRAY_SIZE = 94;
+};
+
+const char* option_str(int tag);
+
+typedef int option_t;
+
+namespace builtin_i {
+  const int colon = 1;
+  const int dot = 2;
+  const int exec_ = 3;
+  const int eval = 4;
+  const int set = 5;
+  const int shift = 6;
+  const int times = 7;
+  const int trap = 8;
+  const int unset = 9;
+  const int builtin = 10;
+  const int readonly = 11;
+  const int local = 12;
+  const int declare = 13;
+  const int typeset = 14;
+  const int export_ = 15;
+  const int test = 16;
+  const int bracket = 17;
+  const int true_ = 18;
+  const int false_ = 19;
+  const int read = 20;
+  const int echo = 21;
+  const int printf = 22;
+  const int mapfile = 23;
+  const int readarray = 24;
+  const int cd = 25;
+  const int pushd = 26;
+  const int popd = 27;
+  const int dirs = 28;
+  const int pwd = 29;
+  const int source = 30;
+  const int umask = 31;
+  const int wait = 32;
+  const int jobs = 33;
+  const int fg = 34;
+  const int bg = 35;
+  const int shopt = 36;
+  const int complete = 37;
+  const int compgen = 38;
+  const int compopt = 39;
+  const int compadjust = 40;
+  const int getopts = 41;
+  const int command = 42;
+  const int type = 43;
+  const int hash = 44;
+  const int help = 45;
+  const int history = 46;
+  const int alias = 47;
+  const int unalias = 48;
+  const int bind = 49;
+  const int push = 50;
+  const int write = 51;
+  const int json = 52;
+  const int repr = 53;
+  const int use = 54;
+  const int opts = 55;
+  const int run = 56;
+  const int fork = 57;
+  const int forkwait = 58;
+  const int cat = 59;
+  const int ARRAY_SIZE = 60;
+};
+
+const char* builtin_str(int tag);
+
+typedef int builtin_t;
+
+
+}  // namespace option_asdl
+
+#endif  // OPTION_ASDL_H
