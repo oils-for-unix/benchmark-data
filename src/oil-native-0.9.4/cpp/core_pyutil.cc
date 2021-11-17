bc35f5c8c42a39eead9554dd013404d79c9bed42
Andy Chu <andy@oilshell.org>
2021-11-17 13:28:57 -0500

    src for oil-native-0.9.4

---
+++ src/oil-native-0.9.4/cpp/core_pyutil.cc
@@ -0,0 +1,41 @@
+// core_pyutil.cc
+
+#include "core_pyutil.h"
+
+namespace pyutil {
+
+bool IsValidCharEscape(int c) {
+  if (c == '/' || c == '.' || c == '-') {
+    return false;
+  }
+  if (c == ' ') {  // foo\ bar is idiomatic
+    return true;
+  }
+  return ispunct(c);
+}
+
+Str* ChArrayToString(List<int>* ch_array) {
+  assert(0);
+}
+
+Str* _ResourceLoader::Get(Str* path) {
+  return new Str("TODO");
+}
+
+_ResourceLoader* GetResourceLoader() {
+  return new _ResourceLoader();
+}
+
+void CopyFile(Str* in_path, Str* out_path) {
+  assert(0);
+}
+
+Str* GetVersion(_ResourceLoader* loader) {
+  return new Str("TODO");
+}
+
+Str* ShowAppVersion(Str* app_name, _ResourceLoader* loader) {
+  assert(0);
+}
+
+}  // namespace pyutil
---
+++ src/oil-native-0.9.4/cpp/libc.h
@@ -0,0 +1,48 @@
+// libc.h: Replacement for native/libc.c
+
+#ifndef LIBC_H
+#define LIBC_H
+
+#include <fnmatch.h>
+#include <unistd.h>  // gethostname()
+
+#include "mylib.h"
+
+namespace libc {
+
+inline Str* gethostname() {
+  char* buf = static_cast<char*>(malloc(HOST_NAME_MAX + 1));
+  int result = ::gethostname(buf, PATH_MAX);
+  if (result != 0) {
+    // TODO: print errno, e.g. ENAMETOOLONG (glibc)
+    throw new RuntimeError(new Str("Couldn't get working directory"));
+  }
+  return new Str(buf);
+}
+
+inline bool fnmatch(Str* pat, Str* str) {
+  // copy into NUL-terminated buffers
+  mylib::Str0 pat0(pat);
+  mylib::Str0 str0(str);
+  int flags = FNM_EXTMATCH;
+  bool result = ::fnmatch(pat0.Get(), str0.Get(), flags) == 0;
+  return result;
+}
+
+List<Str*>* glob(Str* pat);
+
+List<Str*>* regex_match(Str* pattern, Str* str);
+
+Tuple2<int, int>* regex_first_group_match(Str* pattern, Str* str, int pos);
+
+inline void print_time(double real, double user, double sys) {
+  assert(0);
+}
+
+inline Str* realpath(Str* path) {
+  assert(0);
+}
+
+}  // namespace libc
+
+#endif  // LIBC_H
