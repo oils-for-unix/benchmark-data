e6f35220c128d72ab1b67f593babf96287216168
Andy C <andy@lenny>
2022-02-19 16:31:00 -0500

    src for oil-native 0.9.8

---
+++ src/oil-native-0.9.8/mycpp/common.sh
@@ -0,0 +1,16 @@
+#
+# Common functions
+#
+
+# Include guard.
+if test -n "${__MYCPP_COMMON_SH:-}"; then
+  return
+fi
+readonly __MYCPP_COMMON_SH=1
+
+if test -z "${REPO_ROOT:-}"; then
+  echo "$REPO_ROOT should be set before sourcing"
+  exit 1
+fi
+
+readonly MYPY_REPO=$REPO_ROOT/_clone/mypy
---
+++ src/oil-native-0.9.8/mycpp/myerror.h
@@ -0,0 +1,30 @@
+// myerror.h
+//
+// Separate from mylib.h because it has a side of effect of #undef errno, which
+// can't be undone in general.
+
+#ifndef MYERROR_H
+#define MYERROR_H
+
+// Needed for the field below to be valid
+// https://stackoverflow.com/questions/14261534/temporarily-overwrite-a-macro-in-c-preprocessor
+//
+// TODO: Replace with #prgram with something portable
+// I think we just need to rewrite e.errno -> e.errno_ in mycpp.
+
+#pragma push_macro("errno")
+#undef errno
+
+// Base class that mycpp generates.
+class _OSError {
+ public:
+  int errno;
+};
+
+#pragma pop_macro("errno")
+
+class IOError : public _OSError {};
+
+class OSError : public _OSError {};
+
+#endif  // MYERROR_H
