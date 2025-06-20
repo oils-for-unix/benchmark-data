d1aec2893d4d87676b6bbc4eb5edad3a5a9e55fc
Andy C <andy@oilshell.org>
2025-06-19 20:14:26 -0400

    src for Oils 0.30.0

---
+++ src/oils-for-unix-0.30.0/bin/oils_for_unix_preamble.h
@@ -0,0 +1,66 @@
+// bin/oils_for_unix_preamble.h: declarations to run oils_for_unix.mycpp
+
+#include <errno.h>
+#include <fcntl.h>         // e.g. F_DUPFD used directly
+#include <fnmatch.h>       // FNM_CASEFOLD in osh/sh_expr_eval.py
+#include <glob.h>          // GLOB_PERIOD in osh/glob_.py
+#include <regex.h>         // REG_ICASE in osh/sh_expr_eval.py
+#include <sys/resource.h>  // RLIM_INFINITY in builtin/process_osh.py
+#include <sys/wait.h>      // e.g. WIFSIGNALED() called directly
+
+#include "_gen/core/optview.h"
+#include "_gen/core/runtime.asdl.h"
+#include "_gen/core/value.asdl.h"
+#include "_gen/data_lang/nil8.asdl.h"
+#include "_gen/display/pretty.asdl.h"
+#include "_gen/frontend/arg_types.h"
+#include "_gen/frontend/consts.h"
+#include "_gen/frontend/help_meta.h"
+#include "_gen/frontend/id_kind.asdl.h"  // syntax.asdl depends on this
+#include "_gen/frontend/option.asdl.h"
+#include "_gen/frontend/signal.h"
+#include "_gen/frontend/syntax.asdl.h"
+#include "_gen/frontend/types.asdl.h"
+#include "_gen/ysh/grammar_nt.h"
+#include "cpp/core.h"
+#include "cpp/data_lang.h"
+#include "cpp/fanos.h"
+#include "cpp/frontend_flag_spec.h"
+#include "cpp/frontend_match.h"
+#include "cpp/frontend_pyreadline.h"
+#include "cpp/libc.h"
+#include "cpp/osh.h"
+#include "cpp/osh_tdop.h"
+#include "cpp/pgen2.h"
+#include "cpp/pylib.h"
+#include "cpp/stdlib.h"
+#include "cpp/translation_stubs.h"
+#include "mycpp/runtime.h"  // runtime library e.g. with Python data structures
+
+// Function prototypes are emitted in mycpp's DECL phase, and some of them have
+// default args that reuide this.
+
+using id_kind_asdl::Id;
+
+using hnode_asdl::hnode;
+
+using pretty_asdl::doc;
+
+using runtime_asdl::cmd_value;
+using runtime_asdl::part_value;
+using runtime_asdl::scope_e;
+
+using syntax_asdl::command;
+using syntax_asdl::expr;
+using syntax_asdl::loc;
+using syntax_asdl::printf_part;  // added when self._Percent() used it in
+                                 // function signature
+using syntax_asdl::proc_sig;
+using syntax_asdl::suffix_op;
+using syntax_asdl::word;
+using syntax_asdl::word_part;
+
+using types_asdl::lex_mode_e;
+
+using value_asdl::sh_lvalue;  // for builtin_assign.py and builtin_misc.py
+using value_asdl::value;
---
+++ src/oils-for-unix-0.30.0/cpp/core.h
@@ -0,0 +1,130 @@
+// core.h: Replacement for core/*.py
+
+#ifndef CORE_H
+#define CORE_H
+
+#include <pwd.h>  // passwd
+#include <termios.h>
+
+#include "_gen/frontend/syntax.asdl.h"
+#include "cpp/pgen2.h"
+#include "mycpp/runtime.h"
+
+// Hacky forward declaration
+namespace completion {
+class RootCompleter;
+};
+
+namespace pyos {
+
+const int TERM_ICANON = ICANON;
+const int TERM_ECHO = ECHO;
+const int EOF_SENTINEL = 256;
+const int NEWLINE_CH = 10;
+
+Tuple2<int, int> WaitPid(int waitpid_options);
+Tuple2<int, int> Read(int fd, int n, List<BigStr*>* chunks);
+Tuple2<int, int> ReadByte(int fd);
+BigStr* ReadLineBuffered();
+Dict<BigStr*, BigStr*>* Environ();
+int Chdir(BigStr* dest_dir);
+BigStr* GetMyHomeDir();
+BigStr* GetHomeDir(BigStr* user_name);
+
+class ReadError {
+ public:
+  explicit ReadError(int err_num_) : err_num(err_num_) {
+  }
+
+  static constexpr ObjHeader obj_header() {
+    return ObjHeader::ClassFixed(kZeroMask, sizeof(ReadError));
+  }
+
+  int err_num;
+};
+
+class PasswdEntry {
+ public:
+  explicit PasswdEntry(const passwd* entry)
+      : pw_name(StrFromC(entry->pw_name)),
+        pw_uid(entry->pw_uid),
+        pw_gid(entry->pw_gid) {
+  }
+
+  static constexpr ObjHeader obj_header() {
+    return ObjHeader::ClassFixed(field_mask(), sizeof(PasswdEntry));
+  }
+
+  BigStr* pw_name;
+  int pw_uid;
+  int pw_gid;
+
+  static constexpr uint32_t field_mask() {
+    return maskbit(offsetof(PasswdEntry, pw_name));
+  }
+};
+
+List<PasswdEntry*>* GetAllUsers();
+
+BigStr* GetUserName(int uid);
+
+BigStr* OsType();
+
+Tuple2<mops::BigInt, mops::BigInt> GetRLimit(int resource);
+
+void SetRLimit(int resource, mops::BigInt soft, mops::BigInt hard);
+
+Tuple3<double, double, double> Time();
+
+void PrintTimes();
+
+bool InputAvailable(int fd);
+
+List<int>* WaitForReading(List<int>* fd_list);
+
+IOError_OSError* FlushStdout();
+
+Tuple2<int, void*> PushTermAttrs(int fd, int mask);
+void PopTermAttrs(int fd, int orig_local_modes, void* term_attrs);
+
+Tuple2<BigStr*, int>* MakeDirCacheKey(BigStr* path);
+
+bool IsSameFile(BigStr* path1, BigStr* path2);
+
+}  // namespace pyos
+
+namespace pyutil {
+
+double infinity();
+double nan();
+
+bool IsValidCharEscape(BigStr* c);
+BigStr* ChArrayToString(List<int>* ch_array);
+
+class _ResourceLoader {
+ public:
+  _ResourceLoader() {
+  }
+
+  virtual BigStr* Get(BigStr* path);
+
+  static constexpr ObjHeader obj_header() {
+    return ObjHeader::ClassFixed(kZeroMask, sizeof(_ResourceLoader));
+  }
+};
+
+_ResourceLoader* GetResourceLoader();
+
+BigStr* GetVersion(_ResourceLoader* loader);
+
+void PrintVersionDetails(_ResourceLoader* loader);
+
+BigStr* strerror(IOError_OSError* e);
+
+BigStr* BackslashEscape(BigStr* s, BigStr* meta_chars);
+
+grammar::Grammar* LoadYshGrammar(_ResourceLoader*);
+
+}  // namespace pyutil
+
+#endif  // CORE_H
